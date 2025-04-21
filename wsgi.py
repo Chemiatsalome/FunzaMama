f25cba1c9e5f53d5d787eade73829978cc720a7b
chemiatsalome <chemiatsalome@gmail.com>
2025-04-21 19:00:01 +0300

    changes wsgi.py file

--- wsgi.py
+++ wsgi.py
@@ -2,10 +2,10 @@ from app import app
 import os
 
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from Render, fallback to 5000 locally
+    app.run(host="0.0.0.0", port=port)
+
+
 # if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))  # Use PORT from Render, fallback to 5000 locally
-#     app.run(host="0.0.0.0", port=port)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+#     app.run(host="0.0.0.0", port=5000, debug=True)
