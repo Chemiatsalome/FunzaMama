d2a498c5531f85d846748e459e36fd5965c5323a
chemiatsalome <chemiatsalome@gmail.com>
2025-04-21 15:46:48 +0300

    added wsgi.py

--- app.py
+++ app.py
@@ -39,9 +39,5 @@ app.register_blueprint(quiz_bp)
 app.register_blueprint(profile_bp)
 
 
-
-
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000, debug=True)
