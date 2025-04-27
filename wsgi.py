cbb34eb2fda6e1045f63d5cccdfd973933d053e9
chemiatsalome <chemiatsalome@gmail.com>
2025-04-27 20:40:14 +0300

    corrected the wsgi file

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,5 @@
-# wsgi.py
 from app import app
+import os
 
 if __name__ == "__main__":
-    app.run()
-
-
-
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))  # Use PORT from Render, fallback to 5000 locally
-#     app.run(host="0.0.0.0", port=port)
-
-
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
