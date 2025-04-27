46225821c38cfbea6439e19640c0fa650722a2f5
chemiatsalome <chemiatsalome@gmail.com>
2025-04-27 20:03:00 +0300

    Added the index route

--- app.py
+++ app.py
@@ -42,7 +42,10 @@ app.register_blueprint(profile_bp)
 
 import os
 
+# Get the PORT from the environment, default to 5000 if not set
+port = int(os.environ.get('PORT', 5000))
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=port)
 
 
