b957162ae0a5a9b6e18ab954ef30694413a4367c
chemiatsalome <chemiatsalome@gmail.com>
2025-04-27 20:00:10 +0300

    Added the index route

--- app.py
+++ app.py
@@ -43,7 +43,6 @@ app.register_blueprint(profile_bp)
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # fallback port for local dev
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
 
